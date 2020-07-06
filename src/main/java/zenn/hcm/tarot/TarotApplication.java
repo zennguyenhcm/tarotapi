abe1ce77daa3a032123146ac2f28df762956f366
tienbtm <tienbuithimy@admicro.vn>
2020-07-06 18:33:17 +0700

    initial commit

---
+++ src/main/java/zenn/hcm/tarot/TarotApplication.java
@@ -0,0 +1,13 @@
+package zenn.hcm.tarot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TarotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TarotApplication.class, args);
+    }
+
+}
